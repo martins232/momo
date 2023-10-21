159332fb9d2e618ae52b73bbc3d885946a6d23fc
martins <maduekemartinsmartins21@gmail.com>
2023-10-21 08:34:50 +0100

    Cleaned exam

---
+++ exam/factories.py
@@ -0,0 +1,68 @@
+import factory
+from .models import Question, Exam
+from users.models import User
+from teachers . models import Subject
+from  datetime import timedelta
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    # A factory class for the User model
+    class Meta:
+        model = User
+    
+    username = factory.Faker('user_name')
+    email = factory.Faker('email')
+    is_teacher = True # Only create teacher users
+
+class SubjectFactory(factory.django.DjangoModelFactory): # A factory class for the Subject model 
+    class Meta: 
+        model = Subject # Specify the model to use
+
+    name = factory.Faker('word') # Generate a random word as the name
+    teacher = factory.SubFactory(UserFactory) # Create a teacher user for each subject
+    assigned = factory.Faker('boolean') # Generate a random boolean value as the assigned status
+
+class ExamFactory(factory.django.DjangoModelFactory):
+    # A factory class for the Exam model
+    class Meta:
+        model = Exam
+    
+    name = factory.Faker('sentence')
+    subject = factory.SubFactory(SubjectFactory, id=factory.SelfAttribute('..subject_id')) # This will use the subject_id attribute of the exam factory to get or create a subject object
+    # subject = factory.SubFactory(SubjectFactory)
+    duration = timedelta(minutes=60)
+    start_date = factory.Faker('date')  
+
+class QuestionFactory(factory.django.DjangoModelFactory):
+    # A factory class for the Question model
+    class Meta:
+        model = Question
+    
+    teacher = factory.SubFactory(UserFactory) # Create a teacher user for each question
+    exam = factory.SubFactory(ExamFactory) # Create an exam for each question
+    question = factory.Faker('paragraph')
+    option_A = factory.Faker('sentence')
+    option_B = factory.Faker('sentence')
+    option_C = factory.Faker('sentence')
+    option_D = factory.Faker('sentence')
+    answer = factory.Iterator(['A', 'B', 'C', 'D']) # Randomly choose an answer from the options
+    
+    
+# question_obj = QuestionFactory.build()
+
+# # Create and save a question object with random data
+# question = QuestionFactory.create()
+
+# # Create and save a question object with a specific teacher id and exam id
+# question = QuestionFactory.create(teacher_id=2, exam_id=3)
+
+# Create a batch of 70 questions with teacher_id=2 and exam_id=3 and the same question and answer
+questions = QuestionFactory.create_batch(70, teacher_id=2, exam_id=3, question="What is the capital of Nigeria?", answer="A")
+
+# Create a batch of 70 questions with teacher_id=2 and exam_id=3 and random questions and answers
+questions = QuestionFactory.create_batch(70, teacher_id=2, exam_id=3, question=factory.Faker('sentence'), answer=factory.Iterator(['A', 'B', 'C', 'D']))
+
+
+
+# # Create and save a list of 10 questions with random data
+# questions = QuestionFactory.create_batch(10)
--- exam/forms.py
+++ exam/forms.py
@@ -2,7 +2,8 @@
 from django import forms
 from .models import Exam, Question
 from users.models import Grade
-from datetime import date
+from datetime import date, timedelta, datetime
+from django.core.exceptions import ValidationError
 
 
 class ExamForm(forms.ModelForm):
@@ -25,7 +26,7 @@ class ExamForm(forms.ModelForm):
         super(ExamForm, self).__init__(*args, **kwargs)
         self.fields['subject'].queryset = request.user.subject_set.all()
         self.fields["duration"].help_text = "<li>H:M:S</li>"
-        
+        self.fields["duration"].initial = "00:60:00"
         # This will work but when the form selects all teachers that have an active session
         # self.fields['teacher'].initial = request.user
         # self.fields['teacher'].widget.attrs['readonly'] = True
@@ -47,7 +48,21 @@ class ExamForm(forms.ModelForm):
                 }
             ),
         }
+    def clean_duration(self):
+        duration = self.cleaned_data.get("duration")
+        if duration > timedelta(hours=6, minutes=00, seconds=00):
+            raise ValidationError("Duration can't be greater than 6hours")
+        elif duration < timedelta(minutes=5):
+            raise ValidationError("Duration can't be less than 5minutes")
+        else:
+            return duration
     
+    def clean_start_date(self):
+        start_date= self.cleaned_data.get("start_date")
+        two_weeks_from_now = datetime.now() + timedelta(days=14)
+        if start_date.timestamp() >two_weeks_from_now.timestamp():
+            raise ValidationError("The exam start date must be within a two-week window from the current date.")
+        return start_date
 
 class QuestionForm(forms.ModelForm):
     answer_choice =[
@@ -66,12 +81,12 @@ class QuestionForm(forms.ModelForm):
         
     def __init__(self,  *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["question"].widget.attrs.update({"rows":4,}) 
+        self.fields["question"].widget.attrs.update({"rows":5,}) 
         fields= ('option_A', 'option_B', 'option_C', 'option_D', )
         for field in fields:
             self.fields[field].widget.attrs.update({
             "placeholder":f"Answer for {field}",
-            "rows":1,
+            "rows":2,
             "style":"border-radius: 9px;"}) 
         
     
