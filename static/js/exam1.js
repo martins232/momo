f62493e2f466bf03aec374e0fbc904baf15443b8
martins <maduekemartinsmartins21@gmail.com>
2023-11-05 03:28:06 +0100

    stable exam but stiill showing all quest view

---
+++ static/js/exam1.js
@@ -0,0 +1,156 @@
+const url_data = location.origin+examDataUrl
+const usrl_submit = location.origin+examSubmit
+let mydata = ""
+
+var counter;
+    //code for timer
+
+    // Update the count down every 1 second
+    var percentremain=0;
+    var distance = 3600000;
+    var fixed=new Date().getTime();
+    fixed+=distance;
+    var x = setInterval(function() {
+
+    //Test time in milliseconds
+    distance=fixed-(new Date().getTime());
+    percentremain=(distance/36000.0);
+    // Time calculations for days, hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("timer").innerHTML = `<h3 class="text-center">Time remaining:</h3>
+
+            <div class="progress mt-2 fs-4" style="height: 30px; border:2px solid black" >
+                <div class="progress-bar progress-bar-striped py-2" id="bar" style="width:${percentremain}%;">
+                    <span id="timer">${hours} : ${minutes} : ${seconds}</span>
+                </div>
+            </div> `;
+
+    // If the count down is finished, write some text 
+    if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+    counter=61;
+    submitgreen();
+    }
+    }, 1000);
+
+
+
+$.ajax({
+    type: "GET", 
+    url : url_data,
+	async: false, //i did this so that i would be able to save the data gotten from the api into mydata variable above
+    success: function (response) {
+        let data = response.data
+
+        data.forEach(element => { //loop through the data
+			for (const [questions, answers] of Object.entries(element)) {
+				quiz_box.innerHTML += `
+                <hr>
+                <div class="mb-2">
+                
+                    <b>${questions}</b>
+                
+                </div>
+
+                `
+				answers.forEach(answer => { //looop through the answer for each question
+					quiz_box.innerHTML += `
+                    <div>
+                        <label for="${answer}"><input type="radio" name="${questions}" class="form-check-input ans" id="${answer}" value="${answer}"> ${answer}</label>
+                    </div>
+                `
+				})
+
+			}
+		});
+		mydata = response.data
+		console.log(`Inside console.log ${mydata}`)
+    },
+    error: function(error){
+        console.log(error)
+    }
+})
+
+const quizForm = document.getElementById("quiz-form") //get the form that would submit the quiz
+const csrf = document.getElementsByName("csrfmiddlewaretoken") //get csrf token from the from the hidden input
+
+//getElementsBy method always return a collect of elements while getElementby returns just an element
+
+const sendData = () => {
+	const elements = [...document.getElementsByClassName("ans")] // get all the input field with class "ans"
+	const data = {} // create an empty object
+	data["csrfmiddlewaretoken"] = csrf[0].value  // add the csrf token to the data object
+	elements.forEach(el => { 
+		if (el.checked) {   //foreach of the element, if the element has been answered, append the element to the data object with it's answer
+			data[el.name] = el.value
+		} else {  // else if its not checked
+			if (!data[el.name]) { // check if el.name exists in the data object if 'it does not', create it and
+				data[el.name] = null // save it as a null
+			}
+		}
+	})
+
+	$.ajax({
+		type: "POST", // make a post request to the url: student/session/4/save
+		url: usrl_submit, // A string containing the URL to which the request is sent.
+		data: data, // the data object from above to be sent to the server
+		success: function (response) {  // if the post request was sucessful a jsonresponse was created in the view as as this JsonResponse({"pass": True, "score": score_, "result":results})
+			const results = response.result // from the response, get result attribute
+			quizForm.classList.add("d-none") // clear the form element for correction
+
+			results.forEach(res => { // for each of the result
+				const resDiv = document.createElement("div") // create a div element
+				for (const [question, resp] of Object.entries(res)) {
+					resDiv.innerHTML += `${question}<br><hr>`
+					const cls = ["container", "p-3", "text-light", "h3"]
+					resDiv.classList.add(...cls)
+
+					if (resp == "not answered") {
+						resDiv.innerHTML += "-not answered"
+						resDiv.classList.add("bg-danger")
+					}
+					else {
+						const answer = resp["answered"]
+						const correct = resp["correct_answer"]
+
+						if (answer == correct) {
+							resDiv.classList.add("bg-success")
+							resDiv.innerHTML += `answered: ${answer}`
+						} else {
+							resDiv.classList.add("bg-danger")
+							resDiv.innerHTML += `   |correct answer: ${correct}`
+							resDiv.innerHTML += `   |answered: ${answer}`
+						}
+
+					}
+					
+				}
+				document.getElementById("head").innerHTML = "Corrections"
+				const quiz_box = document.getElementById("jaden")
+				quiz_box.appendChild(resDiv)
+				// console.log(resDiv)
+			})
+		},
+		error: function (error) {
+			console.error(error)
+		}
+
+	})
+}
+
+quizForm.addEventListener("submit", e => {
+	e.preventDefault() //prevent the submit button from behaving as it would
+
+	sendData() // call the send button that would send the your request to python
+
+})
+
+console.log(`Outside console.log ${mydata}`)
+
+
+
