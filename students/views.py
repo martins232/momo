e3a882e87828a9b9d2cfc9b295efd0ee56f437f3
martins <maduekemartinsmartins21@gmail.com>
2023-11-07 17:23:47 +0100

    question now fully working as i want

--- students/views.py
+++ students/views.py
@@ -98,46 +98,45 @@ def is_ajax(request): #check if a call is an ajax call
     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
  
 def session_save(request, pk):
-    return HttpResponse("We are comming")
-    # if is_ajax(request=request):
-    #     questions = []
-    #     data =  request.POST
-    #     data_ =dict(data.lists()) #lists() is only available for request.POST method-->, The lists method returns a list of tuples containing the names and values of the input fields.
-    #     data_.pop("csrfmiddlewaretoken") #remove the csrf_token from the dictionary for us to manipulate the question
-    #     for k in data_.keys():
-    #         question = Question.objects.get(question = k)
-    #         questions.append(question) # appending question object in a list
-    #     user = request.user
-    #     exam = Exam.objects.get(id=pk)
+    if is_ajax(request=request):     
+        questions = []
+        data =  request.POST
+        data_ =dict(data.lists()) #lists() is only available for request.POST method-->, The lists method returns a list of tuples containing the names and values of the input fields.
+        data_.pop("csrfmiddlewaretoken") #remove the csrf_token from the dictionary for us to manipulate the question
+        for k in data_.keys():
+            question = Question.objects.get(question = k)
+            questions.append(question) # appending question object in a list
+        user = request.user
+        exam = Exam.objects.get(id=pk)
         
-    #     score = 0
-    #     multiplier = 100 / exam.question_set.all().count()
-    #     results = []
-    #     correct_answer = None
+        score = 0
+        multiplier = 100 / exam.question_set.all().count()
+        results = []
+        correct_answer = None
         
-    #     for q in questions:  # q is the question instance
-    #         a_selected = request.POST.get(q.question) #from the question instance get the question key from the request.POST method
-    #         answer  = {"A" :q.option_A, "B":q.option_B, "C":q.option_C, "D":q.option_D} # represent the options as "A", "B", "C", "D" to correspond to the correct answer from the db
+        for q in questions:  # q is the question instance
+            a_selected = request.POST.get(q.question) #from the question instance get the question key from the request.POST method
+            answer  = {"A" :q.option_A, "B":q.option_B, "C":q.option_C, "D":q.option_D} # represent the options as "A", "B", "C", "D" to correspond to the correct answer from the db
             
-    #         if a_selected != "": # if what the user selected is not empty
-    #             for option, value in answer.items(): # from the answers in the db as dictionary
-    #                 if a_selected in value: # if the student_answer in list of answers (value)
-    #                     if option == q.answer: # if the option is == the teacher answer
-    #                         score += 1  # add a mark
-    #                         correct_answer = value # the student was correct
-    #                     else:
-    #                         correct_answer = answer[q.answer] #the student answer was not correct
-    #             results.append({str(q):{"correct_answer": correct_answer, "answered": a_selected}}) # create dictionary of correct and not correct answer
-    #         else:
-    #             results.append({str(q): "not answered"}) # if the question was not answered create a not answered dictionary
+            if a_selected != "": # if what the user selected is not empty
+                for option, value in answer.items(): # from the answers in the db as dictionary
+                    if a_selected in value: # if the student_answer in list of answers (value)
+                        if option == q.answer: # if the option is == the teacher answer
+                            score += 1  # add a mark
+                            correct_answer = value # the student was correct
+                        else:
+                            correct_answer = answer[q.answer] #the student answer was not correct
+                results.append({str(q):{"correct_answer": correct_answer, "answered": a_selected}}) # create dictionary of correct and not correct answer
+            else:
+                results.append({str(q): "not answered"}) # if the question was not answered create a not answered dictionary
                 
-    #     score_ = score * multiplier # convert to 100% scale
-    #     Session.objects.create(user=user, exam = exam, score=score_) # create and instance of this user session
+        score_ = score * multiplier # convert to 100% scale
+        Session.objects.create(user=user, exam = exam, score=score_) # create and instance of this user session
         
-    #     if score_>=exam.pass_mark:
-    #         return JsonResponse({"pass": True, "score": score_, "result":results}) # create a json response for this user to display data
-    #     else:
-    #         return JsonResponse({"pass": False, "score": score_, "result":results}) # create a json response for this user to display data
+        if score_>=exam.pass_mark:
+            return JsonResponse({"pass": True, "score": score_, "result":results}) # create a json response for this user to display data
+        else:
+            return JsonResponse({"pass": False, "score": score_, "result":results}) # create a json response for this user to display data
 
                 
             
