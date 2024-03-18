186c7a8570b853f308eca56298ab593bb362b790
Martins Madueke <maduekemartins21@gmail.com>
2024-03-18 17:09:47 +0100

    added create superuser in build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 # # To make migrations if this is your first time connecting to a database 
 # python manage.py makemigrations
 
+# Exit on error
 set -o errexit
 
+# Install project dependencies from requirements.txt file
 pip install -r requirements.txt
+
+# Collect static files for deployment
 python manage.py collectstatic --no-input
-#To migrate tables set on your migrations folders
+
+# Create Django superuser if the CREATE_SUPERUSER environment variable is set to True
+if [[ "$CREATE_SUPERUSER" == "True" ]]; then
+  python manage.py createsuperuser --no-input  # Use environment variables for credentials
+fi
+
+
+# Apply database migrations
 python manage.py migrate
