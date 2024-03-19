a312a5e8da7eb6ae00e4283e242e043dcaad55a8
Martins Madueke <maduekemartins21@gmail.com>
2024-03-19 13:23:57 +0100

    make migrations added

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
-# # To make migrations if this is your first time connecting to a database 
-# python manage.py makemigrations
+
+
 
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+
+python manage.py makemigrations
 
 #To migrate tables set on your migrations folders
 python manage.py migrate
